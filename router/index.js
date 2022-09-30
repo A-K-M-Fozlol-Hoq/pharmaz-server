9b2c183800ab9f8458a6782a576e4b131b2e7e69
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-09-30 11:24:17 +0600

    fixed demo router but

--- router/index.js
+++ router/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRouter = require('./userRouter');
-const demoRouter = require('./demoRouter');
 
 router.use('/user', userRouter);
-router.use('/demo', demoRouter);
 
 module.exports = router;
